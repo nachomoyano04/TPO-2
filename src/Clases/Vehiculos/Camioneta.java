2c9fbdbc391df2edf340d2e281cf71cff133259d
SantiagosWorkplace <113646679+SantiagosWorkplace@users.noreply.github.com>
2022-09-16 16:32:04 -0300

    Update Camioneta.java

--- src/Clases/Vehiculos/Camioneta.java
+++ src/Clases/Vehiculos/Camioneta.java
@@ -1,9 +1,17 @@
 package Clases.Vehiculos;
 
-public class Camioneta extends Vehiculo {
+import Clases.Combustible;
 
+public class Camioneta extends Vehiculo {
+    double consumo;
+   
+    public Camioneta(String marca, String patente, Combustible combustible) {
+        super(marca, patente, combustible);
+    }
 
     public double calcularCostoCombustible(){
+        // 10lts por cada 100km
+        
         return 100;
     };
     
