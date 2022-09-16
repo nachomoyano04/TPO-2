8911d3418ca17aea8db924d23a8528a12d779b1b
SantiagosWorkplace <113646679+SantiagosWorkplace@users.noreply.github.com>
2022-09-16 16:53:09 -0300

    Update Viaje.java

--- src/Clases/Viaje.java
+++ src/Clases/Viaje.java
@@ -1,5 +1,79 @@
 package Clases;
 
+import Clases.Vehiculos.Vehiculo;
+
 public class Viaje {
+    public String ciudadOrigen;
+    public String ciudadDestino;
+    public int distancia;
+    public Vehiculo vehiculo;
+    public int cantPeajes;
+    public boolean estanSobreMismaRuta;
+
+    public Viaje(String ciudadOrigen, String ciudadDestino, int distancia, Vehiculo vehiculo, int cantPeajes, boolean estanSobreMismaRuta) {
+        this.ciudadOrigen = ciudadOrigen;
+        this.ciudadDestino = ciudadDestino;
+        this.distancia = distancia;
+        this.vehiculo = vehiculo;
+        this.cantPeajes = cantPeajes;
+        this.estanSobreMismaRuta = estanSobreMismaRuta;
+    }
+
+    public Viaje() {
+    }
+
+    public String getCiudadOrigen() {
+        return ciudadOrigen;
+    }
+
+    public void setCiudadOrigen(String ciudadOrigen) {
+        this.ciudadOrigen = ciudadOrigen;
+    }
+
+    public String getCiudadDestino() {
+        return ciudadDestino;
+    }
+
+    public void setCiudadDestino(String ciudadDestino) {
+        this.ciudadDestino = ciudadDestino;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public int getCantPeajes() {
+        return cantPeajes;
+    }
+
+    public void setCantPeajes(int cantPeajes) {
+        this.cantPeajes = cantPeajes;
+    }
+
+    public boolean isEstanSobreMismaRuta() {
+        return estanSobreMismaRuta;
+    }
+
+    public void setEstanSobreMismaRuta(boolean estanSobreMismaRuta) {
+        this.estanSobreMismaRuta = estanSobreMismaRuta;
+    }
+
+    @Override
+    public String toString() {
+        return "El Viaje{" + "ciudadOrigen=" + ciudadOrigen + ", ciudadDestino=" + ciudadDestino + ", distancia=" + distancia + ", vehiculo=" + vehiculo + ", cantPeajes=" + cantPeajes + ", estanSobreMismaRuta=" + estanSobreMismaRuta + '}';
+    }
     
-}
+    
+ }
