4fb10bf89472c1cef7b0722510c2f9a3f08b1afd
SantiagosWorkplace <113646679+SantiagosWorkplace@users.noreply.github.com>
2022-09-16 17:13:55 -0300

    Update Ciudad.java
    Espero que lean esto OUOUOUOUOUO

--- src/Clases/Ciudad.java
+++ src/Clases/Ciudad.java
@@ -1,5 +1,47 @@
 package Clases;
 
 public class Ciudad {
+    public int ubicacionEnKm;
+    public String sobreRuta;
+    public String nombreCiudad;
+
+    public Ciudad(int ubicacionEnKm, String sobreRuta, String nombreCiudad) {
+        this.ubicacionEnKm = ubicacionEnKm;
+        this.sobreRuta = sobreRuta;
+        this.nombreCiudad = nombreCiudad;
+    }
+
+    public Ciudad() {
+    }
+
+    public int getUbicacionEnKm() {
+        return ubicacionEnKm;
+    }
+
+    public void setUbicacionEnKm(int ubicacionEnKm) {
+        this.ubicacionEnKm = ubicacionEnKm;
+    }
+
+    public String getSobreRuta() {
+        return sobreRuta;
+    }
+
+    public void setSobreRuta(String sobreRuta) {
+        this.sobreRuta = sobreRuta;
+    }
+
+    public String getNombreCiudad() {
+        return nombreCiudad;
+    }
+
+    public void setNombreCiudad(String nombreCiudad) {
+        this.nombreCiudad = nombreCiudad;
+    }
+
+    @Override
+    public String toString() {
+        return "Ciudad{" + "ubicacionEnKm=" + ubicacionEnKm + ", sobreRuta=" + sobreRuta + ", nombreCiudad=" + nombreCiudad + '}';
+    }
+    
     
 }
